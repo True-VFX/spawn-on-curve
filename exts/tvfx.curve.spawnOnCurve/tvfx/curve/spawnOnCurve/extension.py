Rotation fix and code clean up
Fix for spawned objects spawning upside down and removal of unused code

exts/tvfx.curve.spawnOnCurve/tvfx/curve/spawnOnCurve/extension.py
@@ -31,21 +31,13 @@ class Curve:
             if i>1 and i % 3==0:
                 pts_per_curve.append(curve_pts)
                 curve_pts = [pt]
-        
-
-        # for i, crv in enumerate(pts_per_curve):
-        #     print(f"Curve {i}")
-        #     for pt in crv:
-        #         print("    -", pt)
-        
+                
         curves = []
         for crv_pts in pts_per_curve:
             points = np.asfortranarray(crv_pts).T
             crv = bezier.Curve.from_nodes(points)
             curves.append((crv.length, crv))
-        
-        # print("Curves Found:", len(curves))
-        
+                
         return curves
 
     def evaluate(self, fac:float) -> "np.ndarray[float, float, float]":
@@ -120,153 +112,7 @@ class Curve:
 
         return locs, tans
 
-
-
-# class SpawnOnCurve(omni.ext.IExt):
-class _Ignore:
-    def get_active_prim(self) -> Usd.Prim:
-        s = omni.usd.get_context()
-        prim_paths = s.get_selection().get_selected_prim_paths()
-        if not prim_paths:
-            return
-        stage = s.get_stage()
-        return stage.GetPrimAtPath(prim_paths[0])
-    
-    def get_stage(self) -> Usd.Stage:
-        return omni.usd.get_context().get_stage()
-
-    def get_beziers(self, curve_prim:Usd.Prim) -> Curve:
-        pts = curve_prim.GetAttribute("points").Get()
-        return Curve(pts)
-
-    def get_bezier_from_basiscurve(self, curve_prim:Usd.Prim) -> bezier.Curve:
-        b = curve_prim.GetAttribute("points")
-        points = np.asfortranarray(b.Get()).T
-        return bezier.Curve.from_nodes(np.array(points))
-
-    def create_spheres_at_curve_points(self):
-        s = omni.usd.get_context()
-        stage:Usd.Stage = s.get_stage()
-        curve_prim = self.get_active_prim()
-
-        c_points = curve_prim.GetAttribute("points").Get()
-        
-        omni.kit.commands.execute("DeletePrims", paths=test_spheres)
-        stage = self.get_stage()
-        session_layer = stage.GetRootLayer()
-        with Usd.EditContext(stage, session_layer):
-            with Sdf.ChangeBlock():
-                for i,pt in enumerate(c_points):
-                    root_path = stage.GetDefaultPrim().GetPath()
-                    sphere_path = root_path.AppendChild(f"Sphere_{i}")
-                    sphere = session_layer.GetPrimAtPath(sphere_path) or Sdf.PrimSpec(session_layer.GetPrimAtPath(root_path), f"Sphere_{i}", Sdf.SpecifierDef, "Sphere")
-                    pos = session_layer.GetAttributeAtPath(f"{sphere_path}.xformOp:translate") or Sdf.AttributeSpec(sphere, "xformOp:translate", Sdf.ValueTypeNames.Double3)
-                    pos.default = Gf.Vec3d(pt)
-                    radius = session_layer.GetAttributeAtPath(f"{sphere_path}.radius") or Sdf.AttributeSpec(sphere, "radius", Sdf.ValueTypeNames.Double)
-                    radius.default = 10
-                    op_order = session_layer.GetAttributeAtPath(f"{sphere_path}.xformOpOrder") or Sdf.AttributeSpec(sphere, "xformOpOrder", Sdf.ValueTypeNames.TokenArray)
-                    op_order.default  = ["xformOp:translate"]
-                    test_spheres.append(sphere.path)
-
-    def get_quaternion_from_euler(self, roll, pitch, yaw):
-        """
-        Convert an Euler angle to a quaternion.
-        
-        Input
-            :param roll: The roll (rotation around x-axis) angle in radians.
-            :param pitch: The pitch (rotation around y-axis) angle in radians.
-            :param yaw: The yaw (rotation around z-axis) angle in radians.
-        
-        Output
-            :return qx, qy, qz, qw: The orientation in quaternion [x,y,z,w] format
-        """
-        qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-        qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
-        qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
-        qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-        
-        return [qx, qy, qz, qw]
-    
-    def tangent_to_angle(self, tan):
-        base = [1, 0, 0]
-        tan = np.array(tan).flatten()
-        unit_tan = tan / np.linalg.norm(tan)
-        unit_base = base / np.linalg.norm(base)
-        dot_product = np.dot(unit_tan, unit_base)
-        return np.arccos(dot_product)
-
-    def tanget_to_quaternion(self, tan) -> Gf.Quath:
-        angle = self.tangent_to_angle(tan)
-        quat = self.get_quaternion_from_euler(0,0,angle)
-        return Gf.Quath(quat[0], quat[2], quat[1], quat[3])
-
-    def move_cube_along_curve(self, slider:ui.UIntSlider,_b:float):
-        s = omni.usd.get_context()
-        stage:Usd.Stage = s.get_stage()
-        curve_prim = self.get_active_prim()
-        
-        # Ensure Curve
-        if not curve_prim or curve_prim.GetTypeName() != "BasisCurves":
-            return
-
-        # curve = self.get_bezier_from_basiscurve(curve_prim)
-        mcurve = self.get_beziers(curve_prim)
-
-        cube_prim = stage.GetPrimAtPath(stage.GetDefaultPrim().GetPath().AppendPath("Curve_Cube"))
-
-        if not cube_prim:
-            return
-
-        # Move Cube
-        fac = slider.model.get_value_as_float()
-        # loc = mcurve.evaluate(fac).flatten().tolist()
-        loc, tan = mcurve.eval_and_tans(fac)
-        angle = degrees(self.tangent_to_angle(tan))
-        if np.isnan(angle):
-            angle = 0
-        print("Angle:", angle)
-        # quat = self.tanget_to_quaternion(tan)
-        # orig_loc = curve.evaluate(fac).flatten().tolist()
-        UsdGeom.XformCommonAPI(cube_prim).SetTranslate(loc.flatten().tolist())
-        UsdGeom.XformCommonAPI(cube_prim).SetRotate((0,-angle,0))
-
-    # ext_id is current extension id. It can be used with extension manager to query additional information, like where
-    # this extension is located on filesystem.
-    def on_startup(self, ext_id):
-        # Get Selected
-        self._window = ui.Window("Spawn Along Curve", width=300, height=300)
-        with self._window.frame:
-            with ui.VStack():
-                slider = ui.FloatSlider(min=0.0, max=1.0)
-                slider.model.add_value_changed_fn(partial(self.move_cube_along_curve, slider))
-
-                def on_click(slider:ui.FloatSlider):
-                    curve_prim = self.get_active_prim()
-
-                    # Ensure Curve
-                    if curve_prim.GetTypeName() != "BasisCurves":
-                        return
-                    
-                    curve = self.get_bezier_from_basiscurve(curve_prim)
-
-                    # Spawn cube
-                    C:omni.usd.UsdContext = omni.usd.get_context()
-                    stage:Usd.Stage = C.get_stage()
-                    cube_prim: UsdGeom.Cube = UsdGeom.Cube.Define(stage, stage.GetDefaultPrim().GetPath().AppendPath("Curve_Cube"))
-                    # cube_prim: UsdGeom.Cube = UsdGeom.Cube.Define(stage, curve_prim.GetPath().AppendPath("Curve_Cube"))
-                    cube_prim.GetSizeAttr().Set(100.0)
-                    loc = curve.evaluate(slider.model.get_value_as_float()).flatten().tolist()
-                    UsdGeom.XformCommonAPI(cube_prim).SetTranslate(loc)
-
-                ui.Button("Spawn Along Curve", clicked_fn=lambda: on_click(slider), height=20)
-
-                ui.Button("Spheres on Points", clicked_fn=self.create_spheres_at_curve_points, height=20)
-
-    def on_shutdown(self):
-        print("[tvfx.curve.spawnOnCurve] MyExtension shutdown")
-
 test_spheres = []
-# class _Ignore:
 class SpawnOnCurve(omni.ext.IExt):
     def get_quaternion_from_euler(self, roll, pitch, yaw):
         """
@@ -280,10 +126,22 @@ class SpawnOnCurve(omni.ext.IExt):
         Output
             :return qx, qy, qz, qw: The orientation in quaternion [x,y,z,w] format
         """
-        qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-        qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
-        qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
-        qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        roll_2 = roll * 0.5
+        roll_sin = np.sin(roll_2)
+        roll_cos = np.cos(roll_2)
+
+        pitch_2 = pitch * 0.5
+        pitch_sin = np.sin(pitch_2)
+        pitch_cos = np.cos(pitch_2)
+
+        yaw_2 = yaw * 0.5
+        yaw_sin = np.sin(yaw_2)
+        yaw_cos = np.cos(yaw_2)
+
+        qx = roll_sin * pitch_cos * yaw_cos - roll_cos * pitch_sin * yaw_sin
+        qy = roll_cos * pitch_sin * yaw_cos + roll_sin * pitch_cos * yaw_sin
+        qz = roll_cos * pitch_cos * yaw_sin - roll_sin * pitch_sin * yaw_cos
+        qw = roll_cos * pitch_cos * yaw_cos + roll_sin * pitch_sin * yaw_sin
         
         return [qx, qy, qz, qw]
     
@@ -300,14 +158,7 @@ class SpawnOnCurve(omni.ext.IExt):
     
     def get_beziers(self, curve_prim:Usd.Prim, is_closed:bool) -> Curve:
         pts = curve_prim.GetAttribute("points").Get()
-        # print('pts:', tuple(pts[0]),",",type(tuple(pts[0])))
         pts = [tuple(pt) for pt in pts]
-        # print("Point Count Pre:", len(pts))
-        # if is_closed or curve_prim.GetAttribute("wrap").Get() == "nonperiodic":
-        #     print("IS CLOSED")
-        #     pts.append(pts[0])
-        #     pts.append(pts[0])
-        # print("Point Count Post:", len(pts), pts)
         return Curve(pts)
 
     def spawn_along_curve(self, count_slider:ui.UIntSlider, dist_slider:ui.FloatSlider, spawn_at_end:ui.CheckBox, is_closed_checkbox:ui.CheckBox, spawn_type:ui.ComboBox, spawn_obj_path:ui.StringField, _b:float):
@@ -350,7 +201,6 @@ class SpawnOnCurve(omni.ext.IExt):
         p_inst.GetProtoIndicesAttr().Set(ids)
         p_inst.GetPositionsAttr().Set(positions)
         p_inst.GetOrientationsAttr().Set(quats)
-        # print(f"Made {len(ids)} cubes")
 
     def setup_stage(self, curve_prim:Usd.Prim, spawn_obj_path:str) -> UsdGeom.PointInstancer:
         stage = self.get_stage()
@@ -389,15 +239,14 @@ class SpawnOnCurve(omni.ext.IExt):
         return positions, ids, tangents
 
     def tanget_to_quaternion(self, tan) -> Gf.Quath:
-        # tan = tan.flatten()
         base = [1, 0, 0]
         unit_tan = tan / np.linalg.norm(tan)
         unit_base = base / np.linalg.norm(base)
         dot_product = np.dot(unit_tan, unit_base)
         angle = np.arccos(dot_product)
         quat = self.get_quaternion_from_euler(0, -angle, 0)
-        return Gf.Quath(quat[0], quat[1], quat[2], quat[3])
-        # return Gf.Quath(quat[0], quat[2], quat[1], quat[3])
+        return Gf.Quath(quat[3], quat[2], quat[1], quat[0])  # *****************
+        
     def get_active_combo(self, combo_model):
         return [
             combo_model.get_item_value_model(child).as_string
@@ -457,6 +306,7 @@ class SpawnOnCurve(omni.ext.IExt):
                 with ui.HStack():
                     ui.Label("Spawn Object:", height=20,width=100)
                     spawn_obj_path = ui.StringField(height=20)
+                    spawn_obj_path.model.set_value("/World/cone")
                     spawn_obj_path.set_accept_drop_fn(self.drop_accept)
                     spawn_obj_path.set_drop_fn(lambda a, w=spawn_obj_path: self.drop(w, a))
                 